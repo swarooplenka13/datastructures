799bf3faf75238f889ba9bc4f3d1df821998ec20
swarooplenka13 <swarooplenka535@gmail.com>
2021-04-27 20:39:42 +0530

    first commit

---
+++ test/test2 question/test2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    long long int n;
+	    cin>>n;
+	    cout<<n<<endl;
+	}
+	return 0;
+}
+
